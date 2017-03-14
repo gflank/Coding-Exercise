894a553fe023b2b1e13ab62a0ff88a1930b4d5c5
Ziyang Gao <zg84@rutgers.edu>
2017-03-14 17:00:51 -0400

    initial commit

--- 104-Maximum-Depth-Of-Binary-Tree/MaximumDepth.java
+++ 104-Maximum-Depth-Of-Binary-Tree/MaximumDepth.java
@@ -1,45 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    // second solution
-    /*
-    private int max = 1;
-    
-    public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        
-        traversal(root, 0);
-        return max;
-    }
-    
-    private void traversal(TreeNode root, int len) {
-        if (root == null) {
-            return;
-        }
-        ++len;
-        max = Math.max(max, len);
-        
-        traversal(root.left, len);
-        traversal(root.right, len);
-    }
-    */
-    
-    public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
-    }
+HashMap<String, Integer> map = new HashMap<String, Integer>();
+map.put("语文", 1);
+map.put("数学", 2);
+map.put("英语", 3);
+map.put("历史", 4);
+map.put("政治", 5);
+map.put("地理", 6);
+map.put("生物", 7);
+map.put("化学", 8);
+for(Entry<String, Integer> entry : map.entrySet()) {
+	System.out.println(entry.getKey() + ": " + entry.getValue());
 }
